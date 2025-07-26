af22e3e439c5d9d536ea3fe059af17f462e86702
Gary Weng <gweng87@gmail.com>
2025-07-26 17:19:52 +1000

    update manifest

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,32 +1,42 @@
-const CACHE_NAME = 'qr-scanner-cache-v1';
-const URLS_TO_CACHE = [
+// js/service-worker.js
+const CACHE_NAME = 'code-scanner-v1';
+const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/js/jsQR.min.js',
-  // Add other assets as needed (CSS, icons, etc.)
+  '/js/service-worker.js',
+  '/styles.css' // if you have a separate CSS file
 ];
 
-// Install event: cache files
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
   );
 });
 
-// Activate event: cleanup old caches if needed
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Fetch event: serve cached files if offline
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
