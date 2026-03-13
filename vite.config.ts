bd6c75e0e05513f66aa25512d60fc5f3ea071257
Gary Weng <gweng87@gmail.com>
2026-03-13 16:17:27 +1100

    Add initial files for Code Scanner application
    - Created manifest file for PWA support.
    - Added index.html with basic structure and linked assets.
    - Included Tailwind CSS for styling.
    - Added Vite logo SVG for the application icon.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/qr-scanner/",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "docs",
+  },
 });
